b7d807193936c9fdc20acf51237a66bd110af916
Dorel Barbu <dobarbu@deloittece.com>
2020-10-08 12:35:58 +0300

    Add snippets folder

---
+++ Snippets/main.cpp
@@ -0,0 +1,16 @@
+#define INPUT_FILE "date.in"
+#define OUTPUT_FILE "date.out"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ifstream in(INPUT_FILE);
+ofstream out(OUTPUT_FILE);
+
+int main() {
+  
+  return 0;
+}
